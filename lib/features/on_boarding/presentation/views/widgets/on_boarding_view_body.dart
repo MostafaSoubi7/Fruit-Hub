1e4a13afee1a75f53fabe3784f4345242958ff12
MostafaSoubi7 <mm0334471@gmail.com>
2025-02-16 04:30:52 +0200

    page view ui done

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/constants.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -19,7 +21,18 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
             color: AppColors.primaryColor.withOpacity(.5),
           ),
-        )
+        ),
+        const SizedBox(height: 29),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kHorizintalPadding,
+          ),
+          child: CustomButton(
+            onPressed: () {},
+            text: "ابدأ الان",
+          ),
+        ),
+        const SizedBox(height: 43),
       ],
     );
   }
